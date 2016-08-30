8bf7fba1113b2b9d13fa3ce6deeb07ea7857cb1b
[maxximus87] <[maxxturner10@gmail.com]>
2016-08-30 01:00:16 -0400

    ex7

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb." # write this line
+puts "Its fleece was white as #{'snow'}." # write this line and the '' tell Ruby to ignore the variable
+puts "And everywhere that Mary went." # writes this line
+puts "." * 10 # what'd that do? # put ..........
+
+end1 = "C" # defined end1
+end2 = "h" # defined end2
+end3 = "e" # defined end3
+end4 = "e" # defined end4
+end5 = "s" # defined end5
+end6 = "e" # defined end6
+end7 = "B" # defined end7
+end8 = "u" # defined end8
+end9 = "r" # defined end9
+end10 = "g" # defined end10
+end11 = "e" # defined end11
+end12 = "r" # defined end12
+
+# watch that print vs. puts on this line what's it do? # I have no idea!
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12 # these 2 write Cheeseburger
